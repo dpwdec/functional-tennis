11c7006b42994615ec7e2da27fc944ec6b41bd61
dpwdec <declankolakowski@gmail.com>
2020-07-27 11:18:02 +0100

    ceil and floor functionality

--- tennis.js
+++ tennis.js
@@ -16,12 +16,44 @@ const isNumber = pipe(
   charCodeRange
 )
 
-write(isNumber('g'))
-write(isNumber('1'))
-write(isNumber('2'))
-write(isNumber('2'))
+// write(isNumber('g'))
+// write(isNumber('1'))
+// write(isNumber('2'))
+// write(isNumber('2'))
 
-write(['1', '2', 'h', '3'].map(isNumber));
+// write(['1', '2', 'h', '3'].map(isNumber));
+// write(['1', '2', 'h', '3'].filter(isNumber));
+
+const ceil = max => x => x > max ?  max :  x
+const floor = min => x => x < min ? min : x
+
+const ceilTo2 = ceil(2);
+const floorTo1 = floor(1);
+const charToInt = x => parseInt(x)
+
+const numArray = ['0', '7', 'h', '3'].filter(isNumber).map(charToInt)
+
+// why does this error if I just pass in the function directly?
+// const fullNumArray = numArray.map(parseInt) // => fails
+//const fullNumArray = numArray.map(charToInt)
+
+const limitToRange = pipe(
+  ceilTo2,
+  floorTo1
+)
+
+write(limitToRange(numArray).map(ceilTo2).map(floorTo1))
+
+// const makeInput = pipe(
+//   ['0', '7', 'h', '3'].filter(isNumber).map(parseInt),
+//   writeAndReturn,
+//   ceilTo2,
+//   floorTo1
+// )
+
+// write(
+//   makeInput()
+// )
 
 const initialScore = x => y => [x, y]
 const score = initialScore(0)(0)
@@ -43,5 +75,5 @@ const runGame = (score) => {
   });
 }
 
-write('Welcome to a game of tennis');
-runGame(score);
+// write('Welcome to a game of tennis');
+// runGame(score);
