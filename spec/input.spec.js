4e7786c7b0a931dfc1a70a8714fceced556dabc2
dpwdec <declankolakowski@gmail.com>
2020-07-30 15:26:20 +0100

    added test for player 2 string

--- spec/input.spec.js
+++ spec/input.spec.js
@@ -2,7 +2,11 @@
 import { formatInput } from '../input'
 
 describe('formatInput', () => {
-  it('returns 1 when passed  player 1', () => {
+  it('returns 1 when passed player 1', () => {
     expect(formatInput('player 1')).toEqual(1)
+  })
+
+  it('returns 2 when passed player 2', () => {
+    expect(formatInput('player 2')).toEqual(2)
   })
 })
