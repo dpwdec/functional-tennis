d0723e79f03533b846c9f9cc16dea0b1c3910996
dpwdec <declankolakowski@gmail.com>
2020-07-30 15:28:09 +0100

    add additional input tests

--- spec/input.spec.js
+++ spec/input.spec.js
@@ -9,4 +9,12 @@ describe('formatInput', () => {
   it('returns 2 when passed player 2', () => {
     expect(formatInput('player 2')).toEqual(2)
   })
+
+  it('returns 1 when passed arbitrary0', () => {
+    expect(formatInput('arbitrary0')).toEqual(1)
+  })
+
+  it('returns 2 when passed 499201', () => {
+    expect(formatInput('499201')).toEqual(2)
+  })
 })
