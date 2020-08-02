0f3cd42dfb06741bff846cf545471107b9296643
dpwdec <declankolakowski@gmail.com>
2020-08-02 15:06:13 +0100

    add test for fifteen fifteen report case

--- spec/report.spec.js
+++ spec/report.spec.js
@@ -4,4 +4,8 @@ describe('report', () => {
   it('returns love love from 0, 0', () => {
     expect(report([0, 0])).toEqual("love love")
   })
+
+  it('returns fifteen fifteen from [1, 1]', () => {
+    expect(report([1, 1])).toEqual("fifteen fifteen")
+  })
 })
